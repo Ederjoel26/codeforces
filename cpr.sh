5a43792e9d94c7def99e1590328b419b395cbd58
Ederjoel26 <eder.joel55@gmail.com>
2026-02-07 00:45:46 -0600

    Initial commit: Add competitive programming files

---
+++ cpr.sh
@@ -0,0 +1,3 @@
+#!/bin/bash   
+
+alias cpr='g++ -std=c++17 -O2 -Wall -o sol'
---
+++ reverse-permutation.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  int t;
+  cin >> t;
+  
+  while (t--) {
+    int n; cin >> n;
+    vector<int> p(n);
+    
+    for (int& x : p) cin >> x;
+    
+    int i = 0;
+    while (i < n && p[i] == n - i) i++;
+    
+    if (i < n) {
+      auto it = find(p.begin() + i, p.end(), n - i);
+      reverse(p.begin() + i, it + 1);
+    }
+    
+    for (int i = 0; i < n; i++) {
+      cout << p[i] << (i == n-1 ? "\n" : " ");
+    }
+  }
+  
+  return 0;
+}
